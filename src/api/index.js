742b4d8e520cd6a854b0a3f184679bcbd5e398eb
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-03-31 00:12:31 -0700

    testing api calls

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,9 @@ export const getDrivingDistance = async params => {
   try {
     const url = "https://stormy-thicket-48933.herokuapp.com/distance";
     const params = {
-      origins: "5454 Rosewood Street, Montclair, CA 91763",
-      destinations: "138 S Adams St, Glendale"
-    }
+      origins: "Glendale",
+      destinations: "Burbank"
+    };
     const { data } = await axios.post(url, params);
     console.log("data:", data);
   } catch (e) {
